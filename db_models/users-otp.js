8139ed6f7f697dac560238e8b4ed4cf4d901b857
Ravi Shankar <sravi4701@gmail.com>
2019-06-18 01:03:10 +0530

    add user otp schema

---
+++ db_models/users-otp.js
@@ -0,0 +1,18 @@
+const conn = require('../modules/db');
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userOtpSchema = new Schema({
+    user_id: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+        unique: true
+    },
+    otp: {
+        type: String,
+        unique: true
+    }
+});
+
+module.exports = conn.model('users', userOtpSchema);
